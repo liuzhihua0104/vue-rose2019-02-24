d4f9f0c6a1161bc910cb4270e6402d92eb8f7117
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-02-24 23:14:35 +0800

    创建vue练习项目“

---
+++ src/components/BaseInstruct.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-cloak>{{cloak}}</div>
+    <div v-text="msg"></div>
+    <div v-html="html"></div>
+    <div v-bind:title="title">v-bind:title后边的内容会被当做js表达式来解析; v-bind:title简写为 :title”</div>
+    <h2 @click="alert">click me</h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cloak: "我是v-cloak渲染的，为了解决插值表达式闪烁的问题",
+      msg: "这是v-text渲染的",
+      html: "<h2>我是v-html渲染的</h2>",
+      title: "我是v-bind-title绑定的动态属性"
+    };
+  },
+  methods: {
+    alert() {
+      alert("v-on:click绑定可以简写为“@click”");
+      console.log(this)
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
+
+
