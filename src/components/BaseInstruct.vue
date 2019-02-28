073ff56d2a7bdc605592c16f2e09a12ca95276c3
liuzhihua <liuzhihua@lushangchuangfu.com>
2019-02-28 23:11:49 +0800

    生命周期钩子函数

--- src/components/BaseInstruct.vue
+++ src/components/BaseInstruct.vue
@@ -39,6 +39,46 @@ export default {
       clearInterval(this.interval);
       this.interval = null;
     }
+  },
+
+  //初始化实例对象，还未初始化data和methods中的方法
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+
+  // data和methods已经初始化好了，可以操作数据，最早可以在这里调用一些方法了
+  created() {
+    console.log("created");
+  },
+
+  // 模板已经在内存中编译好了，但是还没有挂在到页面中
+  beforeMount() {
+    console.log("beforeMount");
+  },
+
+  // 内存中的模板已经挂在到页面中了
+  mounted() {
+    console.log("mounted");
+  },
+
+  // 数据已更行，页面未更新
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+
+  // 页面上数据更新完成
+  updated() {
+    console.log("updated");
+  },
+
+  // 组件销毁之前（网上看的：router-view加了个keep-alive导致组件缓存可能不触发此函数）
+  beforeDestory() {
+    console.log("beforeDestory");
+  },
+
+  // 组件已销毁（网上看的：router-view加了个keep-alive导致组件缓存可能不触发此函数）
+  destoryed() {
+    console.log("destoryed");
   }
 };
 </script>
